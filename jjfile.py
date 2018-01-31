13f016536ca99e56f6677a91f146f9ca69fd1b76
yotsuyanagi <yotsuyanagi@torico-tokyo.com>
2018-01-31 18:25:28 +0900

    pypi upload

--- jjfile.py
+++ jjfile.py
@@ -9,12 +9,6 @@ menu = [
      ["./test-dump-meta.sh; ./test-dump-toc.sh; ./test-extract-jpeg.sh"]),
     ('flake8',
      "flake8 ."),
-    ('register pypi',
-     "twine register dist/epub-extractor-0.1.4.tar.gz"),
     ('upload pypi',
-     "twine upload dist/epub-extractor-0.1.4.tar.gz"),
-    ('__register pypi',
-     "./setup.py register"),
-    ('__upload pypi',
-     "./setup.py sdist upload"),
+     "./setup.py sdist; twine upload dist/*"),
 ]
